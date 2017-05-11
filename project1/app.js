099bb29d71294b012aac9ca3f1e68ba86c459cea
Dhananjay Singh <dhananjay1204@gmail.com>
2017-05-12 03:35:34 +0530

    Added https-server and eslint

--- project1/app.js
+++ project1/app.js
@@ -3,5 +3,14 @@ var myApp = angular.module('angularApp', []);
  
 // CONTROLLERS
 myApp.controller('mainController', function ($scope) {
+    var a=10;
+    var b="10";
+    if(a=="100"){
+        console.log("a==b");
+    }
+    if(a===b){
+        console.log("a===b");
+    }
   
 });
+
